79b0a1a0fbe379be683f59e06079ba8e32d2c0c7
vaishnavikchari <vaishnavichari@gmail.com>
2020-02-03 18:18:27 +0530

    updated email service

--- src/components/Five.js
+++ src/components/Five.js
@@ -1,62 +1,116 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Fade from 'react-reveal/Fade';
+import emailjs from 'emailjs-com';
 
-const Five = props => (
-  <section id="five" className="wrapper style2 special fade inactive">
-    <Fade duration={2500}>
-      <div className="container">
-        <header>
-          <h2>Let's get started</h2>
-          <p>Message us, now</p>
-        </header>
-        <form
-          method="post"
-          action="#"
-          className="cta"
-          id="usrform"
-          data-netlify="true"
-        >
-          <div className="row gtr-uniform gtr-50">
-            <div className="col-8 col-12-xsmall">
-              <input
-                aria-label="Your name"
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your First Name"
-              />
+function Five () {
+  const [name, setName] = useState ('');
+  const [email, setEmail] = useState ('');
+  const [message, setMessage] = useState ('');
+  const [isSent, setIsSent] = useState (false);
 
-              <input
-                aria-label="Your email"
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Your Email Address"
-              />
+  function sendEmail (e) {
+    e.preventDefault ();
 
-              <textarea
-                aria-label="Your message"
-                form="usrform"
-                rows="4"
-                cols="50"
-                name="message"
-                id="message"
-                placeholder="Your Message"
-              />
+    emailjs
+      .sendForm (
+        'gmail_sriveda',
+        'template_sriveda',
+        e.target,
+        'user_u7YCIHfTwT6M9vdlC862P'
+      )
+      .then (
+        result => {
+          setIsSent (true);
+        },
+        error => {}
+      );
+  }
+
+  const emailForm = (
+    <section id="five" className="wrapper style2 special fade inactive">
+      <Fade duration={2500}>
+        <div className="container">
+          <header>
+            <h2>Let's get started</h2>
+            <p>Message us, now</p>
+          </header>
+          <form
+            method="post"
+            action="#"
+            onSubmit={sendEmail}
+            className="cta"
+            id="usrform"
+            data-netlify="true"
+          >
+            <div className="row gtr-uniform gtr-50">
+              <div className="col-8 col-12-xsmall">
+                <input
+                  aria-label="Your name"
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={e => setName (e.target.value)}
+                  id="name"
+                  placeholder="Your First Name"
+                />
+
+                <input
+                  aria-label="Your email"
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={e => setEmail (e.target.value)}
+                  id="email"
+                  placeholder="Your Email Address"
+                />
+
+                <textarea
+                  aria-label="Your message"
+                  form="usrform"
+                  rows="4"
+                  cols="50"
+                  name="message"
+                  value={message}
+                  onChange={e => setMessage (e.target.value)}
+                  id="message"
+                  placeholder="Your Message"
+                />
+              </div>
+              <div className="col-4 col-12-xsmall">
+                <input
+                  aria-label="Submit the form"
+                  id="submitForm"
+                  type="submit"
+                  value="Submit"
+                  className="fit primary"
+                />
+                <div style={{color: 'yellow'}}>{}</div>
+              </div>
             </div>
-            <div className="col-4 col-12-xsmall">
-              <input
-                aria-label="Submit the form"
-                id="submitForm"
-                type="submit"
-                value="Submit"
-                className="fit primary"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
-    </Fade>
-  </section>
-);
+          </form>
+        </div>
+      </Fade>
+    </section>
+  );
+
+  const thankyouMessage = (
+    <section id="five" className="wrapper style2 special fade inactive">
+      <Fade duration={2500}>
+        <div className="container">
+          <header>
+            <h2>Let's get started</h2>
+            <p>Message us, now</p>
+            <hr />
+            <br />
+            <p>We've received your message</p>
+            <h2>Thank you!</h2>
+            <p>We will revert to you very soon</p>
+          </header>
+        </div>
+      </Fade>
+    </section>
+  );
+
+  return isSent ? thankyouMessage : emailForm;
+}
 export default Five;
